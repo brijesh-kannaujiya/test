44ddc91c843095a0ad1eee961214a42c6ad5f31c
BRIJESH <brijesh@BRIJESHs-MacBook-Air.local>
2022-04-30 15:59:49 +0530

    first

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '641e8c38e5eca3db01400f8fc437456f30f447ab',
+        'name' => 'brijesh/test',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'brijesh/test' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '641e8c38e5eca3db01400f8fc437456f30f447ab',
+            'dev_requirement' => false,
+        ),
+    ),
+);
